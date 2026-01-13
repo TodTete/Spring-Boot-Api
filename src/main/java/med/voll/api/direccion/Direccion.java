4a17372e64950cce3afa6079f39dd08a95d24afb
TodTete <vallejoricardo3@gmail.com>
2026-01-12 22:22:36 -0600

    DTO #2

--- src/main/java/med/voll/api/direccion/Direccion.java
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -1,5 +1,8 @@
 package med.voll.api.direccion;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public class Direccion {
   private  String calle;
   private  String numero;
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,10 +1,28 @@
 package med.voll.api.medico;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.direccion.Direccion;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "medicos")
+@Entity(name = "Medico")
 public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre, email, documento;
+
+    @Enumerated(EnumType.STRING)
     private Especialidad especialidad;
+
+    @Embedded
     private Direccion direccion;
 }
