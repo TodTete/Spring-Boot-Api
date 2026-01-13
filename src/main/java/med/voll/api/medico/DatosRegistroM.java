8599bbeb2bca63731703818e7b1b19fa0eeb22c9
TodTete <vallejoricardo3@gmail.com>
2026-01-12 23:24:40 -0600

    New DTQ

--- src/main/java/med/voll/api/medico/DatosRegistroM.java
+++ src/main/java/med/voll/api/medico/DatosRegistroM.java
@@ -2,6 +2,11 @@ package med.voll.api.medico;
 
 import med.voll.api.direccion.DatosDireccion;
 
-public record DatosRegistroM(String nombre, String email, String documento, Especialidad especialidad, DatosDireccion direccion) {
+public record DatosRegistroM(
+        String nombre,
+        String email,
+        String documento,
+        Especialidad especialidad,
+        DatosDireccion direccion) {
 
 }
