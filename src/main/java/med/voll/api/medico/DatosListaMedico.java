90b877b5f541dc0a44dcf0b532d0c27be1c0f901
TodTete <vallejoricardo3@gmail.com>
2026-01-13 22:41:19 -0600

    Constructor

---
+++ src/main/java/med/voll/api/medico/DatosListaMedico.java
@@ -0,0 +1,9 @@
+package med.voll.api.medico;
+
+public record DatosListaMedico(
+        String nombre, String email, String documento, Especialidad especialidad) {
+
+    public DatosListaMedico(Medico medico) {
+        this(medico.getNombre(), medico.getEmail(), medico.getDocumento(), medico.getEspecialidad());
+    }
+}
